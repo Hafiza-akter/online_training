16790f97ad9af3e04aca44c4883f64228f00cff0
Hafiza-akter <emuhsbd@gmail.com>
2021-01-30 11:39:29 +0600

    plan purchase module

---
+++ app/Model/PurchasePlan.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PurchasePlan extends Model
+{
+    protected $table = 'tbl_purchase_plans';
+}
---
+++ resources/views/admin/plan_purchase/list.blade.php
@@ -0,0 +1,92 @@
+@extends('admin/master')
+@section('title','Plan List')
+@section('pageName','Plan list')
+@section('content')
+<div class="container-fluid">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+        <div class="col-sm">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+
+                        <div class="col-sm-6">
+                            <h3 class="card-title">Purchased Plan List</h3>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    @if(Session::has('message'))
+                    <p id="flashMessage" class="alert {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
+                    @endif
+                    <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>#sl</th>
+                                <th> Name</th>
+                                <th> Objective</th>
+                                <th> Times Per Week</th>
+                                <th> Cost Per Month</th>
+                                <th> Percentage One Month</th>
+                                <th> Percentage Two Month</th>
+                                
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php $i=1 @endphp
+                            @foreach($planList as $plan)
+                            <tr>
+                                <td>{{$i++}}</td>
+                                <td>{{$plan->name}}</td>
+                                <td>{{$plan->objective}}</td>
+                                <td>{{$plan->times_per_week}}</td>
+                                <td>{{$plan->cost_per_month}}</td>
+                                <td>{{$plan->percentage_1mo}}</td>
+                                <td>{{$plan->percentage_3mo}}</td>
+                               
+                                <td>
+                                    <?php 
+                                    if($plan->status == 1){ ?>
+                                        <input type="button"  readonly  class="btn btn-primary"  value=" Active ">
+                                    <?php }else{?>
+                                        <input type="button"  readonly  class="btn btn-warning"  value="Inactive">
+                                  <?php  } ?>
+                                </td>                                
+                                <td>
+                                    <a href="{{route('purchase.plan.edit',$plan->id)}}" class="pl-3 pr-3"><i class="fas fa-edit"></i></a>
+                                </td>
+
+                            </tr>
+                            @endforeach
+
+
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                            <th>#sl</th>
+                                <th> Name</th>
+                                <th> Objective</th>
+                                <th> Times Per Week</th>
+                                <th> Cost Per Month</th>
+                                <th> Percentage One Month</th>
+                                <th> Percentage Two Month</th>
+                                
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+        <!-- /.row -->
+
+    </div><!-- /.container-fluid -->
+
+
+    @endsection
