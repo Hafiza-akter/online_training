248ab12d04d6984053e4329afb16853c2c7eea2a
leekwangsoo1995 <jayzisgodminion@gmail.com>
2021-04-12 19:37:03 +0900

    Update index_v2.blade.php

--- resources/views/pages/toppage/index_v2.blade.php
+++ resources/views/pages/toppage/index_v2.blade.php
@@ -8,10 +8,10 @@
                     <div class="banner_text text-center">
                         <div class="banner_text_iner">
                             <!-- <h2>Join <span>now</span> </h2> -->
-                            <p>現在テスト運用中です。
-サービスは利用できませんが
-テスター（ユーザー）ご希望の方は
-「ユーザー登録」から登録願います。</p>
+                        <p style="font-size:40px; padding-bottom:25px;padding-top:10px">    事前登録受付中</p>
+                        <p style="padding-bottom:15px">現在サービスは利用できませんが、サービス開始前のテストユーザーとトレーナーを募集中です。</p>
+                        <p style="padding-bottom:15px">時期によって無償対応や割引がございますが抽選によります。また、正式開始時にはメールでご連絡致します。</p>
+                        <p style="padding-bottom:10px">ご希望の方は下のボタンよりご登録ください。</p>
                         <h2>
                             <span></span>
                             <span>
